4950820e70fcd62318e6693d2d3ebac8f232c65a
Steven Myint <git@stevenmyint.com>
2013-05-26 16:19:57 -0700

    Add installer

---
+++ setup.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+
+"""Setup for pydiff."""
+
+from __future__ import unicode_literals
+
+import ast
+from distutils import core
+
+
+def version():
+    """Return version string."""
+    with open('pydiff.py') as input_file:
+        for line in input_file:
+            if line.startswith('__version__'):
+                return ast.parse(line).body[0].value.s
+
+
+with open('README.rst') as readme:
+    core.setup(name='pydiff',
+               version=version(),
+               description='Diff two Python files at the bytecode level.',
+               long_description=readme.read(),
+               license='Expat License',
+               author='Steven Myint',
+               url='https://github.com/myint/pydiff',
+               classifiers=['Intended Audience :: Developers',
+                            'Environment :: Console',
+                            'Programming Language :: Python :: 2.7',
+                            'Programming Language :: Python :: 3',
+                            'Programming Language :: Python :: 3.2',
+                            'Programming Language :: Python :: 3.3',
+                            'License :: OSI Approved :: MIT License'],
+               keywords='diff, bytecode, python, whitespace, formatting',
+               py_modules=['pydiff'],
+               scripts=['pydiff'])
